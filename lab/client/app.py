16d8c10baab269ce402e5af67ff956482d4c8d4a
Abenezer Mamo <1840417+AbenezerMamo@users.noreply.github.com>
2021-04-08 08:44:55 -0700

    C:\Users\abene\Documents\Lab\100\Lab\client

--- lab/client/app.py
+++ lab/client/app.py
@@ -1,14 +1,40 @@
 import os
 import sys
 import subprocess
-import time
 
-subprocess.call(['git', 'add', '-A'])
-time = str(time.localtime())
+"""
+Return the current status of the git repo
+"""
+try:
+    subprocess.call(['git', 'status'])
+except:
+    print("Git not initalized")
+
+"""
+Add the local changes to be ready for a commit
+"""
+try:
+    subprocess.call(['git', 'add', '-A'])
+except:
+    print("There changes in have been made in this directory")
+
+"""
+Commits the latest changes
+"""
+for p in sys.path:
+    subprocess.call(['git', 'commit', '-m', p])
 
 
+"""
+If destination is set for both fetch & push, expect the username to go into global variable
+"""
+subprocess.call(['git', 'remote', '-v'])
 
-subprocess.call(['git', 'commit', '-m', 'd'])
+"""
+Push the changes
+"""
+# subprocess.call(['git', 'push'])
+
 # for p in sys.path:
 #     print(p)
 #
