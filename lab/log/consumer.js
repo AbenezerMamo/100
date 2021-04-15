b12d331f464336a02858c6c262d7185b600609d6
Abenezer Mamo <1840417+AbenezerMamo@users.noreply.github.com>
2021-04-15 14:32:00 -0700

    working on basic definition for redis

---
+++ lab/log/consumer.js
@@ -0,0 +1,56 @@
+// Consumer outline
+
+/*
+Set of heuristics we'll use for the end-to-end "tracing" of an event to a metric
+
+Profile.updateChanges(
+{
+  {
+  "repo":
+    {
+      repo_name:
+        {
+        "commits":
+         {
+          authored_date,
+          message,
+          repo_id
+        },
+
+
+    },
+    {"commits": {
+    authored_date,
+    message,
+    repo_id,
+    relative_reference_to_HEAD,
+    tag
+  }
+}
+}
+
+)
+COMMIT ID
+Also known as SHA. A 40-character checksum hash that identifies the commit.
+
+*/
+// Appending a new set of
+
+
+
+
+/*
+References:
+http://modeling
+
+-languages.com/wp-content/uploads/2015/08/schema-v5.png
+*/
+
+// Relationships
+/*
+LINE_NUMBER : FILE
+
+
+COMMIT_STREAKS_BOARD : DATE
+
+*/
---
+++ lab/log/publisher.js
@@ -0,0 +1,19 @@
+// Redis
+
+
+// Logging the frequency of a log surfacing
+ZSET
+// Union transformations
+
+// Response
+// MIN & MAX from combined set
+
+// Incrementing keys
+
+// Five commands for operating on multiple keys
+
+
+// Update stats
+
+
+//
