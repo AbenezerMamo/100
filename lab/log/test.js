bb69c6e24140161403bd810275b50d1168dd7b7a
Abenezer Mamo <1840417+AbenezerMamo@users.noreply.github.com>
2021-04-16 21:02:46 -0700

    testing setup

--- lab/log/test.js
+++ lab/log/test.js
@@ -21,3 +21,40 @@ Redis_Module_blockClient()
 REDISMODULE_Notify() for broadcasting alerts
 
 */
+
+
+/*
+
+Input validation
+
+Verify:
+Make a request to grab the user's ownership of repo
+
+Log of each activity
+
+Premissions from Auth:
+Code
+Issues
+Pull requests
+Wikis
+Settings
+Webhooks and services
+Deploy keys
+Collaboration invites
+
+
+
+
+webhooks
+1. Notify
+Sngle delivery
+
+
+2. Sync
+Retry
+
+
+
+
+
+*/
